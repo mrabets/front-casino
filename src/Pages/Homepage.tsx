e1df2c9323b64c9a64ea00b0ab66af1d3bd9431f
Karim Marabet <newrabgo@gmail.com>
2022-01-25 17:51:38 +0300

    Init commit

---
+++ src/Pages/Homepage.tsx
@@ -0,0 +1,7 @@
+export function Homepage() {
+  return (
+    <div>
+      <h1>Hello Man</h1>
+    </div>
+  );
+}
---
+++ src/types/data.ts
@@ -0,0 +1,5 @@
+export interface IUserSliceState {
+  user_id: string | null;
+  email: string | null;
+  token: string | null; 
+}
